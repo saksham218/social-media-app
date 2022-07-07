8daafbe4deb66184656d09c2d6b416aed361c67e
saksham218 <sakshamkumar03218@gmail.com>
2022-07-07 12:54:14 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+//every route will start with /posts/..... 
+//i.e. we added a prefix of /posts/ to each route
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
+
+
+const CONNECTION_URL = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
